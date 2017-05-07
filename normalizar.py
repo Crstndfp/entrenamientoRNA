17f6c33289fb263dd4ba386201ad31c113cd5ee7
rockael72 <rockael_16@hotmail.com>
2017-05-06 20:28:27 -0600

    Otro cambio

--- normalizar.py
+++ normalizar.py
@@ -3,14 +3,13 @@ import numpy as np
 import scipy as sp
 import os
 
-cont = 0
 
 def normalized(a, axis=-1, order=2):
     l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
     l2[l2==0] = 1
     return a / np.expand_dims(l2, axis)
 
-def mandar(archivo):
+def mandar(archivo, salida):
     archivo_entrenamiento = open("normalizado.csv", "a")
     global cont
     for x1 in range(len(archivo[:, 0])):
@@ -19,10 +18,8 @@ def mandar(archivo):
             b = str(normalizado[0, x2])
             archivo_entrenamiento.write(b[:b.find(".")+5])
             archivo_entrenamiento.write(" ")
-        if (cont==0):
-            archivo_entrenamiento.write("0.09 0.0 0.0 0.0 0.0")
-        if (cont==1):
-            archivo_entrenamiento.write("0.09 0.0 0.0 0.0 0.0")
+
+        archivo_entrenamiento.write(salida)
         archivo_entrenamiento.write("\n")
     archivo_entrenamiento.close()
 
@@ -33,7 +30,7 @@ if(os.path.exists("normalizado.csv")== True):
     os.remove("normalizado.csv")
 dato = np.matrix(sp.genfromtxt("tomates-recortados-bueno.csv", delimiter=" "))
 dato2 = np.matrix(sp.genfromtxt("tomates-recortados-malos.csv", delimiter=" "))
-
-mandar(dato)
-cont=cont+1
-mandar(dato2)
+salida1 = "0.09 0 0 0 0 0"
+salida2 = "0 0.09 0 0 0 0"
+mandar(dato,salida1)
+mandar(dato2,salida2)
